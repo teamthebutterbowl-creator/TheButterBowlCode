5acd9127238107a6c632db5b9ab7f8e1eb8aa3c0
ashutosh <aaa.shutoshvashishth@gmail.com>
2026-06-17 02:11:38 +0530

    Changes in resend email

--- userSide/Backend/utils/resendEmail.js
+++ userSide/Backend/utils/resendEmail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendEmail = async ({ to, subject, html }) => {
   try {
     const response = await resend.emails.send({
-      from: "Your App <onboarding@resend.dev>",
+      from: "The Butter Bowl <hello@thebutterbowl.in>",
       to,
       subject,
       html,
